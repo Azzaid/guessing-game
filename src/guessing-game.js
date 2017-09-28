9d6d0716db47f43291745889de683312f7c511c3
Azzaid <johanas.azzaid@gmail.com>
2017-09-28 22:10:23 +0300

    ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.upperLimit = NaN;
+        this.lowerLimit = NaN;
+        this.currentGuess = NaN;
+    }
 
     setRange(min, max) {
-
+        this.upperLimit = max;
+        this.lowerLimit = min;
     }
 
     guess() {
-
+        this.currentGuess = Math.round(this.lowerLimit + (this.upperLimit - this.lowerLimit)/2);
+        console.log("предполагаю " + this.currentGuess);
+        return(this.currentGuess);
     }
 
     lower() {
-
+        this.upperLimit = this.currentGuess;
     }
 
     greater() {
-
+        this.lowerLimit = this.currentGuess;
     }
 }
 
